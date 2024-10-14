876de1e59dcceb22e0ee71a673e903f8ad573728
heislerlemos <heisler.stlano@gmail.com>
2024-10-14 17:09:24 +0100

    trabalhando no get method

--- erps/urls.py
+++ erps/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('erps/', views.erps, name="erps")
+    path('', views.erps, name="erps")
 ]
--- erps/views.py
+++ erps/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from erps.models import Rh
 
 def erps(request):
+    
+    rhs = Rh.objects.all().values()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    context = {
+    'myerps' : rhs,
+    }
+    return HttpResponse(template.render(context, request))
 
 
