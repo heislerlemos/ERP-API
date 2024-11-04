7e2b1ef2afb8220aff503cd015b6cc4e533bac29
heislerlemos <heisler.stlano@gmail.com>
2024-11-04 15:33:40 +0100

    adicionado função serializer author Fanuel Ramos

--- erps/api.py
+++ erps/api.py
@@ -1,8 +1,16 @@
 from tastypie import fields
 from tastypie.resources import ModelResource
+from tastypie.serializers import Serializer
 from erps.models import Rh
+
+class SerializedData(Serializer):
+    def to_json(self, data, options=None):
+        data = data['objects']
+        return super (SerializedData, self).to_json(data, options)
+
 
 class RhResource(ModelResource):
     class Meta: 
         queryset = Rh.objects.all()
         resource_name = 'rh'
+        serializer = SerializedData()
